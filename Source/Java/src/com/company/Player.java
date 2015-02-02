9f14f577b8110147f8a20861889c4cb4e7f5667d
sadbas <bas.dk@hotmail.com>
2015-02-02 11:25:20 +0100

    Added propeties and Methods

--- Source/Java/src/com/company/Player.java
+++ Source/Java/src/com/company/Player.java
@@ -3,6 +3,25 @@ package com.company;
 /**
  * Created by Sadik on 02-02-2015.
  */
+public class Player  {
+    public String name;
+    public int age;
+    private Character[] characters;
 
-public class Player {
+
+    public void play() {
+
+    }
+public void addCharacter() {
+    }
+
+
+    public void deleteCharacter(Character character) {
+
+    }
+
+    public void chooseCharacter(Character character) {
+
+    }
+
 }
