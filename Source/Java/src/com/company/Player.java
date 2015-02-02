43ecb9c5e3b4d632c80046a455a650643654130f
Christian Mitteldorf <christian.km@me.com>
2015-02-02 11:41:12 +0100

    Reformatted code

--- Source/Java/src/com/company/Player.java
+++ Source/Java/src/com/company/Player.java
@@ -3,25 +3,22 @@ package com.company;
 /**
  * Created by Sadik on 02-02-2015.
  */
-public class Player  {
+
+public class Player {
+
     public String name;
     public int age;
     private Character[] characters;
 
-
     public void play() {
-
-    }
-public void addCharacter() {
     }
 
+    public void addCharacter() {
+    }
 
     public void deleteCharacter(Character character) {
-
     }
 
     public void chooseCharacter(Character character) {
-
     }
-
 }
