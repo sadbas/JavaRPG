3625d63479fd8175016c8d009f3ee44b426b2f70
Christian Mitteldorf <christian.km@me.com>
2015-02-02 12:37:10 +0100

    Fixed and reformatted code

--- Source/Java/src/com/company/Character.java
+++ Source/Java/src/com/company/Character.java
@@ -1,33 +1,30 @@
 package com.company;
 
+import com.company.BaseCharacter;
 import com.company.Enumerations.Direction;
 
 /**
  * Created by Sadik on 02-02-2015.
  */
+
 public class Character extends BaseCharacter {
+
     private Inventory inventory;
-    private gear Map<GearPosition, Item>;
+    private Map<GearPosition, Item> gear;
 
     public void setName(String name) {
-        this.name =name;
+        this.name = name;
     }
 
-public void move(Direction) {
-
+    public void move(Direction direction) {
     }
 
     public boolean attack() {
     }
 
-
-    public void useSkill(Skill skill){
-
-}
-
+    public void useSkill(Skill skill) {
+    }
 
     public void drinkPotion() {
-
-
-}
+    }
 }
