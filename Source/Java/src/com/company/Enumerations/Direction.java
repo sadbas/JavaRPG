69b996ecea8481752a877559d93d79d7fa3f4f9a
Christian Mitteldorf <christian.km@me.com>
2015-02-02 12:51:23 +0100

    Added documentation

--- Source/Java/src/com/company/Enumerations/Direction.java
+++ Source/Java/src/com/company/Enumerations/Direction.java
@@ -4,6 +4,9 @@ package com.company.Enumerations;
  * Created by christian on 02/02/15.
  */
 
+/**
+ * The Direction-enum defines directions that a character can move.
+ */
 public enum Direction {
     North,
     East,
