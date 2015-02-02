c8790a0ecf2bc9b8f55318d8bda910998753c7fd
Christian Mitteldorf <christian.km@me.com>
2015-02-02 12:51:40 +0100

    Added documentation

--- Source/Java/src/com/company/Helpers/Origin.java
+++ Source/Java/src/com/company/Helpers/Origin.java
@@ -4,7 +4,13 @@ package com.company.Helpers;
  * Created by christian on 02/02/15.
  */
 
+/**
+ * This class represents an origin on the map, with x- and y coordinates.
+ * The point [0,0] defines the upper left corner of the screen.
+ */
 public class Origin {
+    /** The horizontal position in the coordinate system. */
     public int x;
+    /** The vertical position in the coordinate system. */
     public int y;
 }
