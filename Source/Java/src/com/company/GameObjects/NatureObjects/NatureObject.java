b1afb8011ad2b58205d9e02034d5fc71e08a9323
Christian Mitteldorf <christian.km@me.com>
2015-02-03 14:01:42 +0100

    Added base class for static Nature Objects

---
+++ Source/Java/src/com/company/GameObjects/NatureObjects/NatureObject.java
@@ -0,0 +1,11 @@
+package com.company.GameObjects.NatureObjects;
+
+import com.company.GameObjects.GameObject;
+import com.company.GameObjects.IInteraction;
+
+/**
+ * Created by christian on 03/02/15.
+ */
+
+public abstract class NatureObject extends GameObject implements IInteraction {
+}
