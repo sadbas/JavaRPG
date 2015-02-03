8530d50b95414f94cd2af57fb56572631a160cc5
Christian Mitteldorf <christian.km@me.com>
2015-02-03 14:47:43 +0100

    Added Interface for Interaction between Game Objects

---
+++ Source/Java/src/com/company/GameObjects/IInteraction.java
@@ -0,0 +1,11 @@
+package com.company.GameObjects;
+
+/**
+ * Created by christian on 03/02/15.
+ */
+
+public interface IInteraction {
+
+    public boolean isPassable();
+    public boolean isAttackable();
+}
