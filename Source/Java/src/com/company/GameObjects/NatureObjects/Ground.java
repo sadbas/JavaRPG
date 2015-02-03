055a84a81abdb691b396975f2ea0f63aea4064ee
Christian Mitteldorf <christian.km@me.com>
2015-02-03 14:02:38 +0100

    Added class for representing the ground

---
+++ Source/Java/src/com/company/GameObjects/NatureObjects/Ground.java
@@ -0,0 +1,19 @@
+package com.company.GameObjects.NatureObjects;
+
+/**
+ * Created by christian on 03/02/15.
+ */
+
+/**
+ * This class represents the ground.
+ */
+public class Ground extends NatureObject {
+
+    public boolean isPassable() {
+        return true;
+    }
+
+    public boolean isAttackable() {
+        return false;
+    }
+}
