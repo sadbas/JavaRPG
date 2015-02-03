1ab48ef3a62f4636184a33b325417bc5e47b79b8
Christian Mitteldorf <christian.km@me.com>
2015-02-03 14:03:29 +0100

    Added Toolbox-class containing helper-methods that does not fit elsewhere.

---
+++ Source/Java/src/com/company/Helpers/Toolbox.java
@@ -0,0 +1,14 @@
+package com.company.Helpers;
+
+/**
+ * Created by christian on 02/02/15.
+ */
+
+public class Toolbox {
+
+    public static void clear() {
+        for (int i = 0; i < 30; i++) {
+            System.out.println();
+        }
+    }
+}
