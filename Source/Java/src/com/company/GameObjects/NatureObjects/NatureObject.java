4005d1a750ced8ecc89856d1725945d79c85120f
Christian Mitteldorf <christian.km@me.com>
2015-02-03 14:02:08 +0100

    Added documentation

--- Source/Java/src/com/company/GameObjects/NatureObjects/NatureObject.java
+++ Source/Java/src/com/company/GameObjects/NatureObjects/NatureObject.java
@@ -7,5 +7,8 @@ import com.company.GameObjects.IInteraction;
  * Created by christian on 03/02/15.
  */
 
+/**
+ * The NatureObject class describes a static game object that cannot move.
+ */
 public abstract class NatureObject extends GameObject implements IInteraction {
 }
