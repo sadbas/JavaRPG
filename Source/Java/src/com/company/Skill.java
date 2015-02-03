a53357176d1ed6a9b5dcde228ff41491728d6b9d
Christian Mitteldorf <christian.km@me.com>
2015-02-03 09:31:40 +0100

    Added Skill-class to represent a Skill.

---
+++ Source/Java/src/com/company/Skill.java
@@ -0,0 +1,21 @@
+package com.company;
+
+/**
+ * Created by christian on 02/02/15.
+ */
+
+public class Skill {
+
+    /** The name of the skill. */
+    private String name;
+    /** The damage is defined as a percentage of the characters damage. */
+    private float damage;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public float getDamage() {
+        return this.damage;
+    }
+}
