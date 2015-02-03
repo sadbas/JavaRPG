b2ef15dc57a827327f76779a502849df6217fe56
Christian Mitteldorf <christian.km@me.com>
2015-02-03 14:02:51 +0100

    Added class for representing a Rock

---
+++ Source/Java/src/com/company/GameObjects/NatureObjects/Rock.java
@@ -0,0 +1,19 @@
+package com.company.GameObjects.NatureObjects;
+
+/**
+ * Created by christian on 03/02/15.
+ */
+
+/**
+ * This class represents a Nature object in the form of a Rock.
+ */
+public class Rock extends NatureObject {
+
+    public boolean isPassable() {
+        return false;
+    }
+
+    public boolean isAttackable() {
+        return false;
+    }
+}
