d7d2d10750a36c09ed83c33750713d8db917626d
Christian Mitteldorf <christian.km@me.com>
2015-02-04 18:45:52 +0100

    Added documentation.

--- Source/Java/src/com/company/Helpers/Console.java
+++ Source/Java/src/com/company/Helpers/Console.java
@@ -4,33 +4,46 @@ package com.company.Helpers;
  * Created by christian on 02/02/15.
  */
 
+/**
+ * The Console-class provides methods for writing and reading from the console.
+ */
 public abstract class Console {
 
     //region Constants
     private final static int ROWS = 30;
     //endregion
 
+    /**
+     * Prints a string.
+     *
+     * @param s The string to print.
+     */
     public static void print(String s) {
         System.out.print(s);
     }
 
+    /**
+     * Prints a string followed by a linebreak.
+     *
+     * @param s The string to print.
+     */
     public static void println(String s) {
         System.out.println(s);
     }
 
+    /**
+     * Prints an empty line.
+     */
     public static void newln() {
         System.out.println();
     }
 
+    /**
+     * Imitates clearing the console, by writing blank lines.
+     */
     public static void clear() {
         for (int i = 0; i < ROWS; i++) {
             System.out.println();
-        }
-    }
-
-    public static void black() {
-        for (int i = 0; i < ROWS; i++) {
-            System.out.println("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
         }
     }
 }
