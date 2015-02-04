7e23ceb5dbde68bb45f9bc25d90d690efbf2a1d2
Christian Mitteldorf <christian.km@me.com>
2015-02-04 09:13:40 +0100

    Added constructor

--- Source/Java/src/com/company/Monster.java
+++ Source/Java/src/com/company/Monster.java
@@ -1,17 +1,18 @@
 package com.company;
 
 /**
- * Created by WIN4 on 03-02-2015.
+ * Created by Sadik on 03-02-2015.
  */
-public class Monster extends BaseCharacter{
+
+public class Monster extends BaseCharacter {
+
     //private Item[] loot;
     private Gold gold;
 
-    public Monster(){
-        this.name="Monster"
-    this.gold=new Gold();
-        this.health=new Health(100,100);
-        this.damage=5
-
+    public Monster() {
+        this.name = "Monster";
+        this.gold = new Gold();
+        this.health = new Health(100, 100);
+        this.baseDamage = 2;
     }
 }
