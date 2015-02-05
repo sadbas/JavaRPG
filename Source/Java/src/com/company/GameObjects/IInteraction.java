2532a17460c6218cd5200ca0c46f6cb904bac9af
Christian Mitteldorf <christian.km@me.com>
2015-02-05 10:40:43 +0100

    Added documentation

--- Source/Java/src/com/company/GameObjects/IInteraction.java
+++ Source/Java/src/com/company/GameObjects/IInteraction.java
@@ -4,8 +4,22 @@ package com.company.GameObjects;
  * Created by christian on 03/02/15.
  */
 
+/**
+ * The Interaction-interface contains methods that defines how the object implementing this interface, must react to certain interactions or events, that can occur when playing the game.
+ */
 public interface IInteraction {
 
+    /**
+     * Returns a boolean value determining if the object can be passed.
+     *
+     * @return True if object can be passed, or false.
+     */
     public boolean isPassable();
+
+    /**
+     * Returns a boolean value determining if the object can be attacked.
+     *
+     * @return True if the object can be attacked, or false.
+     */
     public boolean isAttackable();
 }
