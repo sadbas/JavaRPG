404f592414817d959a9d3987623568abc4a355a7
Christian Mitteldorf <christian.km@me.com>
2015-02-05 11:07:38 +0100

    Added documentation

--- Source/Java/src/com/company/Combat/ICombat.java
+++ Source/Java/src/com/company/Combat/ICombat.java
@@ -4,9 +4,17 @@ package com.company.Combat;
  * Created by christian on 04/02/15.
  */
 
+/**
+ * The Combat-interface contains methods that defines how the object implementing this interface, can react to attacks in combat.
+ */
 public interface ICombat {
 
-    public Attack attack();
-    public int skill1();
-    public int skill2();
+    /**
+     * The implementation of this method describes how the character reacts to,
+     * and defends an attack, and returns the real damage actually dealt.
+     *
+     * @param attack The attack to be defended.
+     * @return A float-value of real damage dealt, which is also the health lost.
+     */
+    public float defend(Attack attack);
 }
