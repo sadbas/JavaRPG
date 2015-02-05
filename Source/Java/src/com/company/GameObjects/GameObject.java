fb463069e973f88a3e882f3526177140317c072b
Christian Mitteldorf <christian.km@me.com>
2015-02-05 10:44:04 +0100

    Added documentation

--- Source/Java/src/com/company/GameObjects/GameObject.java
+++ Source/Java/src/com/company/GameObjects/GameObject.java
@@ -4,5 +4,9 @@ package com.company.GameObjects;
  * Created by christian on 03/02/15.
  */
 
+/**
+ * This class is an abstract class describing a general Game Object.
+ * All objects used in the game, living and nature, must be a subclass this class.
+ */
 public abstract class GameObject implements IInteraction {
 }
