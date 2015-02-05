70eedd88be15f849f8330f75e9b9951193cb29df
Christian Mitteldorf <christian.km@me.com>
2015-02-05 12:50:06 +0100

    Added documentation

--- Source/Java/src/com/company/GameScene.java
+++ Source/Java/src/com/company/GameScene.java
@@ -1,8 +1,9 @@
 package com.company;
 
+import com.company.Combat.CombatScene;
 import com.company.Enumerations.Direction;
 import com.company.GameObjects.GameObject;
-import com.company.Helpers.Toolbox;
+import com.company.Helpers.Console;
 import com.company.Map.Map;
 import com.company.Map.MapObject;
 
@@ -14,6 +15,9 @@ import java.util.Scanner;
  * Created by christian on 02/02/15.
  */
 
+/**
+ * The Game scene controls the main flow in the game, and is responsible for keeping track of the map and rendering it. It also handles movement and other user actions.
+ */
 public class GameScene {
 
     private static final Scanner input = new Scanner(System.in);
@@ -22,7 +26,7 @@ public class GameScene {
     private Map map;
 
     /**
-     * Start a game with the provided character.
+     * Start a new game with the provided character.
      *
      * @param character A character to be played.
      */
@@ -38,42 +42,73 @@ public class GameScene {
         }
     }
 
+    /**
+     * This is the main game loop, which renders the map, and performs actions based on player input.
+     */
     private void update() {
 
         // Update map from last turn
-        Toolbox.clear();
+        Console.clear();
         renderMap();
 
         // Players Turn
-        System.out.println();
-        System.out.println("Use WASD-keys to move around");
-        System.out.println("What's your move?: ");
+        Console.newln();
+        Console.println("Use WASD-keys to move around");
+        Console.println("What's your move?: ");
         String action = input.next();
 
-        Point newPoint = new Point();
+        GameObject destinationObject = null;
+
+        // Move
         // TODO Use Switch (Not available on OS X - Java SDK 1.6)
         if (action.equals(GameAction.MoveUp.toString())) {
-            this.map.move(character, Direction.North);
-        }
-        else if (action.equals(GameAction.MoveLeft.toString())) {
-            this.map.move(character, Direction.West);
-        }
-        else if (action.equals(GameAction.MoveDown.toString())) {
-            this.map.move(character, Direction.South);
-        }
-        else if (action.equals(GameAction.MoveRight.toString())) {
-            this.map.move(character, Direction.East);
+            destinationObject = this.map.move(character, Direction.North);
+        } else if (action.equals(GameAction.MoveLeft.toString())) {
+            destinationObject = this.map.move(character, Direction.West);
+        } else if (action.equals(GameAction.MoveDown.toString())) {
+            destinationObject = this.map.move(character, Direction.South);
+        } else if (action.equals(GameAction.MoveRight.toString())) {
+            destinationObject = this.map.move(character, Direction.East);
         }
 
         // Enter Combat-mode if destination point contains Monster
-        GameObject obj = (GameObject)this.map.objectAtPoint(newPoint).getObject();
-        /*if (obj.getClass() == Monster.getClass()) {
+        if (destinationObject instanceof Monster) {
+            Console.newln();
+            Console.println("A wild monster appeared!");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Monster monster = (Monster) destinationObject;
+
             // Initialize Combat Scene
-            CombatScene combatScene = new CombatScene(character, obj);
-            combatScene.fight();
-        }*/
+            CombatScene combatScene = new CombatScene(character, monster);
+            boolean won = combatScene.fight();
+
+            if (won) {
+                // Remove monster
+                this.map.removeObject(monster);
+            } else {
+                // Die
+                character.die();
+                this.map.resetCharacter();
+
+                Console.clear();
+                Console.println("      You Died.");
+                Console.newln();
+                Console.newln();
+                Console.println("Press any key + Enter to continue..");
+
+                // Wait for input
+                input.next();
+            }
+        }
     }
 
+    /**
+     * Renders the map, and prints it to the console.
+     */
     private void renderMap() {
         for (int y = 0; y < this.map.numberOfRows(); y++) {
             String row = "";
@@ -82,7 +117,7 @@ public class GameScene {
                 MapObject obj = this.map.objectAtPoint(p);
                 row += obj.getKey();
             }
-            System.out.println(row);
+            Console.println(row);
         }
     }
 }
