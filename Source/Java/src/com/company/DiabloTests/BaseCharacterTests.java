c0cba2464d43d017244df850f83f95dfb53dd43f
Christian Mitteldorf <christian.km@me.com>
2015-02-16 13:19:54 +0100

    Added DiabloTests Package

---
+++ Source/Java/src/com/company/DiabloTests/BaseCharacterTests.java
@@ -0,0 +1,19 @@
+package com.company.DiabloTests;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BaseCharacterTests {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testIsDead() throws Exception {
+
+    }
+}
